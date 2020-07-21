c75ff20f023b24b7efcf53241d6148152ccae596
Hareesh Perumal S <hariselvaraj1998@gmail.com>
2020-07-21 10:59:19 +0530

    simple test was been added

---
+++ src/test/java/TestAssertion.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestAssertion {
+  @Test
+    public void just(){
+      assertEquals(7,7);
+  }
+}
